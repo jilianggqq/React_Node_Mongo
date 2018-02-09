5faebb89d01484d2afcb6020bed993050230ae47
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2018-02-08 23:32:54 -0800

    add keys.js for development

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+// dev.js - production keys here
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+// 463580672547-54gu6r9b282t8gu90th4tse3gqh7eamo.apps.googleusercontent.com
+// 751NALYchJ_ZWe-yqElXZAKs
