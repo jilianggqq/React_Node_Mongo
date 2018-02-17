53fc15b93f64d121c87048c70fc804bd421edcf6
Qiqiang@ituBig <qguan@itu.edu>
2018-02-16 18:35:22 -0800

    configure redux with first step

--- client/Tech.md
+++ client/Tech.md
@@ -7,3 +7,9 @@
 
 1. proxy copies whole requests.
 2. proxy sends request to api.
+
+### React-Redux library
+
+React-Redux library makes react and Redux working together.
+Provider is a binding of react and redux.
+some elements(like listitem) can access to redux store using Provider tag.
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
 import App from "./components/App";
+import reducers from "./reducers";
+
+// we will use createStore help to create instance of our redux store.
+// 1st param is all the different producers that we have inside of our application.
+// 2ed param is initial state of our application.
+const store = createStore(reducers, {}, applyMiddleware());
 
 // 2 arguments.
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  // Provider is a react component that read changes from our redux store.
+  // anytime the store has some change, the Provider will inform all of its chirdren components.
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
