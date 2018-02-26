09a7d40cac7bc29b63a7658e66c7f27b9b93a4d1
Qiqiang@ituBig <qguan@itu.edu>
2018-02-25 18:59:55 -0800

    add configuration for produciton mode in heroku

--- index.js
+++ index.js
@@ -65,5 +65,17 @@ require("./routes/billingRoutes")(app);
 // if the app is running in local, process.env.PORT === undefined.
 const PORT = process.env.PORT || 5000;
 
+// if app is on the production(Heroku)
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets
+  // like main.js file or main.css file
+  app.use(express.static("client/build"));
+  // express will serve up the index.html file if it doesn't recognize the route.
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.listen(PORT);
 console.log("app is running in port " + PORT);
